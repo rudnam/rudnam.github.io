6d3fe3d77c72341bc848d3364b1b395d9908c8da
rudnam <francisalbarracin2@gmail.com>
2024-03-16 20:20:01 +0800

    feat: Set up website

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <span onClick={() => location.reload()}>rudnam</span>
+      </header>
+      <main>
+        <p>{"This doesn't really have anything right now."}</p>
+        <p>工事中です。</p>
+      </main>
+    </div>
+  );
+}
+
+export default App;
