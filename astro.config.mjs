d9c97ab10d68faaf9c00424400d463499f6c5bc3
rudnam <francisalbarracin2@gmail.com>
2025-07-26 19:24:49 +0800

    Update code blocks

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,18 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import rehypeImageZoom from "./src/rehype/rehype-image-zoom";
+import rehypeCopyButton from "./src/rehype/rehype-copy-button";
 
 export default defineConfig({
   site: "https://rudnam.github.io",
   integrations: [mdx(), sitemap(), tailwind()],
   markdown: {
-    rehypePlugins: [rehypeImageZoom],
+    rehypePlugins: [rehypeImageZoom, rehypeCopyButton],
+    shikiConfig: {
+      themes: {
+        light: "github-light",
+        dark: "github-dark",
+      },
+    },
   },
 });
--- src/rehype/rehype-image-zoom.js
+++ src/rehype/rehype-image-zoom.js
@@ -1,9 +1,8 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import { visit } from "unist-util-visit";
 
 export default function rehypeImageZoom() {
   return (tree) => {
-    visit(tree, "element", (node, _index, _parent) => {
+    visit(tree, "element", (node) => {
       if (node.tagName === "img") {
         node.properties = node.properties || {};
         node.properties.class = (node.properties.class || "") + " zoomable-img";
